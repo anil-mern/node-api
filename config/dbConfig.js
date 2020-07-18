83d790f153e3f2d5043a6153d29897676d0a8a45
Anil Bomma <s537161@nwmissouri.edu>
2020-07-18 17:23:20 -0500

    updated db config creating index to  avoid duplicate entries

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,7 +12,7 @@ const connectionOptions = {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-  autoIndex: false, // Don't build indexes
+  // autoIndex: false, // Don't build indexes
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
--- utils/dsMgr.js
+++ utils/dsMgr.js
@@ -1,21 +1,27 @@
-let save = async(modal) => {
-    try {
-        let save = await modal.save();
-        return save;
-    } catch (err) {
-        return err;
-    }
-}
+let save = async (modal) => {
+  try {
+    let save = await modal.save();
 
-let findOne = async(modal, query, select) => {
-    try {
-        let modalData = await modal.findOne(query).select(select).lean().exec();
-        return modalData;
-    } catch (err) {
-        return err;
+    if (save instanceof mongoose.Error) {
+      throw save;
     }
+    return save;
+  } catch (err) {
+    throw err;
+  }
+};
 
-}
+let findOne = async (modal, query, select) => {
+  try {
+    let modalData = await modal.findOne(query).select(select).lean().exec();
+    if (modalData instanceof mongoose.Error) {
+      throw modalData;
+    }
+    return modalData;
+  } catch (err) {
+    return err;
+  }
+};
 
 module.exports.save = save;
 module.exports.findOne = findOne;
